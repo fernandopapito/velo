fa1dfb67748af614680d091530bd8b1b3b0e9149
Fernando Papito <fernandopapito@proton.me>
2026-01-27 14:35:47 -0300

    Timeouts, estratégias de validações e isolamento da massa de testes

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,21 +3,29 @@ import { test, expect } from '@playwright/test'
 /// AAA - Arrange, Act, Assert
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+
+  // Test Data
+  const order = 'VLO-6E2J20'
+
   // Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-  
+
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act  
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-6E2J20')
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
   await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10_000})
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-6E2J20')
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  const containerPedido = page.getByRole('paragraph')
+    .filter({ hasText: /^Pedido$/ })
+    .locator('..') //Sobe para o elemento pai (a div que agrupa ambos)
+
+  await expect(containerPedido).toContainText(order, { timeout: 10_000 })
+
+  await expect(page.getByText('APROVADO')).toBeVisible()
+
 })
