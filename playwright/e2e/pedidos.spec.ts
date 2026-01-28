572e90b018b7dab0ac583d3c0d1fb21a9b18be00
Fernando Papito <fernandopapito@proton.me>
2026-01-28 16:49:56 -0300

    hooks, massa dinamica e cobertura de testes

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,31 +1,57 @@
 import { test, expect } from '@playwright/test'
 
+import { generateOrderCode } from '../support/helpers'
+
 /// AAA - Arrange, Act, Assert
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
+test.describe('Consulta de Pedido', ()=> {
 
-  // Test Data
-  const order = 'VLO-6E2J20'
+  test.beforeEach(async ({page}) => {
+    // Arrange
+    await page.goto('http://localhost:5173/')
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+  })
+  
+  test('deve consultar um pedido aprovado', async ({ page }) => {
 
-  // Arrange
-  await page.goto('http://localhost:5173/')
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+    // Test Data
+    const order = 'VLO-6E2J20'
+  
+    // Act  
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+  
+    // Assert
+  
+    const containerPedido = page.getByRole('paragraph')
+      .filter({ hasText: /^Pedido$/ })
+      .locator('..') //Sobe para o elemento pai (a div que agrupa ambos)
+  
+    await expect(containerPedido).toContainText(order, { timeout: 10_000 })
+  
+    await expect(page.getByText('APROVADO')).toBeVisible()
+  
+  })
+  
+  test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
+  
+    const order = generateOrderCode()
+  
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+  
+  
+    await expect(page.locator('#root')).toMatchAriaSnapshot(`
+      - img
+      - heading "Pedido não encontrado" [level=3]
+      - paragraph: Verifique o número do pedido e tente novamente
+      `)
+  
+  })
 
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-
-  // Act  
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
-  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
-
-  // Assert
-
-  const containerPedido = page.getByRole('paragraph')
-    .filter({ hasText: /^Pedido$/ })
-    .locator('..') //Sobe para o elemento pai (a div que agrupa ambos)
-
-  await expect(containerPedido).toContainText(order, { timeout: 10_000 })
-
-  await expect(page.getByText('APROVADO')).toBeVisible()
 
 })
+
